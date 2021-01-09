1e83f1be6763fafbd441ff4cabd4cfb2ba2b7530
liwm29 <905529001@qq.com>
2021-01-09 23:49:11 +0800

    add deploy server

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	// 注意这里,涉及go mod的本地包导入,如果是子文件夹的包,不需要在子包也go mod init
+	// 引入子包的方法: 比如go mod init module_name,则引入module_name/package_dir_name,即子包的文件夹名,但引入后,用的是go文件头部package_name
+	backend "server/backend"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func index(ctx *gin.Context) {
+	ctx.Header("Content-Type", "text/html")
+	ctx.File("frontend/dist/index.html")
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", index)
+	backend.InitRouter(r)
+	r.Static("static", "frontend/dist/static")
+	r.StaticFile("favicon.ico", "frontend/dist/favicon.ico")
+	r.Run(":12345")
+}
